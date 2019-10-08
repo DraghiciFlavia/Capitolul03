3307ef8eaad3a94d48b3ba7527681de86314986a
DraghiciFlavia <55551803+DraghiciFlavia@users.noreply.github.com>
2019-10-08 19:57:29 +0300

    up

--- Fibonsci/main.cpp
+++ Fibonsci/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    unsigned short a = 1,b =1, c;
+    unsigned short nr;
+    cout << "Scrieti un numar " << endl;
+    cin >> nr;
+
+    do
+    {
+       c = a + b;
+       a = b;
+       b = c;
+
+    }while (c <= nr );
+
+    cout << "Nr din sirul Fibonnaci cel mai aproape de " << nr << " este " << (c == nr ? c:a) << endl;
     return 0;
 }
